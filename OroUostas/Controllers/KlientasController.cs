4c32f4ae36b6a2a054392caad0688227344e71ce
MartynasCepas <martynas.cepas@ktu.edu>
2020-04-23 16:07:09 +0300

    Added CRUD operations for klientas

--- OroUostas/Controllers/KlientasController.cs
+++ OroUostas/Controllers/KlientasController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OroUostas.Models;
 using OroUostas.Reps;
 
 namespace OroUostas.Controllers
@@ -13,6 +14,101 @@ namespace OroUostas.Controllers
         public ActionResult Index()
         {
             return View(klientaiRepository.getKlientai());
+        }
+
+        // GET: Klientas/Create
+        public ActionResult Create()
+        {
+            Klientas klientas = new Klientas();
+            return View(klientas);
+        }
+
+        // POST: Klientas/Create
+        [HttpPost]
+        public ActionResult Create(Klientas collection)
+        {
+            try
+            {
+                // Patikrinama ar klientas su tokiu asmens kodu jau egzistuoja
+                Klientas tmpKlientas = klientaiRepository.getKlientas(collection.asmens_kodas);
+                if (tmpKlientas.asmens_kodas!=0)
+                {
+                    ModelState.AddModelError("asmens_kodas", "Klientas su tokiu asmens kodu jau užregistruotas");
+                    return View(collection);
+                }
+
+                collection.id = klientaiRepository.getNewId();
+                //Jei nera sukuria nauja klienta
+                if (ModelState.IsValid)
+                {
+                    klientaiRepository.addKlientas(collection);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(collection);
+            }
+        }
+
+        // GET: Klientas/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(klientaiRepository.getKlientas(id));
+        }
+
+        // POST: Klientas/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                bool naudojama = false;
+
+           //     if (klientaiRepository.getKlientasSutarciuCount(id)>0)
+           //     {
+           //         naudojama = true;
+           //         ViewBag.naudojama = "Negalima pašalinti klientas turėjo sudarytų sutarčių";
+           //         return View(klientasRepository.getKlientas(id));
+           //     }
+
+                
+                klientaiRepository.deleteKlientas(id);
+                
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Klientas/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(klientaiRepository.getKlientas(id));
+        }
+
+        // POST: Klientas/Edit/5
+        [HttpPost]
+        public ActionResult Edit(string id, Klientas collection)
+        {
+            try
+            {
+                // Atnaujina kliento informacija
+                if (ModelState.IsValid)
+                {
+                    klientaiRepository.updateKlientas(collection);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(collection);
+            }
         }
     }
 }
--- OroUostas/Reps/KlientaiRepository.cs
+++ OroUostas/Reps/KlientaiRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Web.Mvc;
 using OroUostas.Models;
 using MySql.Data.MySqlClient;
 
@@ -37,6 +38,144 @@ namespace OroUostas.Reps
             }
 
             return klientai;
+        }
+
+        public Klientas getKlientas(long kodas)
+        {
+            Klientas klientas = new Klientas();
+            string conn = ConfigurationManager.ConnectionStrings["MysqlConnection"].ConnectionString;
+            MySqlConnection mySqlConnection = new MySqlConnection(conn);
+            string sqlquery = "select * from " + "klientai where asmens_kodas="+kodas;
+            MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+            mySqlCommand.Parameters.Add("?asmens_kodas", MySqlDbType.Int64).Value = kodas;
+            mySqlConnection.Open();
+            MySqlDataAdapter mda = new MySqlDataAdapter(mySqlCommand);
+            DataTable dt = new DataTable();
+            mda.Fill(dt);
+            mySqlConnection.Close();
+
+            foreach (DataRow item in dt.Rows)
+            {
+                klientas.id = Convert.ToInt32(item["kliento_id"]);
+                klientas.asmens_kodas = Convert.ToInt64(item["asmens_kodas"]);
+                klientas.vardas = Convert.ToString(item["vardas"]);
+                klientas.pavarde = Convert.ToString(item["pavarde"]);
+                klientas.amzius = Convert.ToInt32(item["amzius"]);
+                klientas.telefono_numeris = Convert.ToString(item["telefono_numeris"]);
+                klientas.elektroninis_pastas = Convert.ToString(item["elektroninis_pastas"]);
+            }
+            return klientas;
+        }
+
+        public Klientas getKlientas(int kodas)
+        {
+            Klientas klientas = new Klientas();
+            string conn = ConfigurationManager.ConnectionStrings["MysqlConnection"].ConnectionString;
+            MySqlConnection mySqlConnection = new MySqlConnection(conn);
+            string sqlquery = "select * from " + "klientai where kliento_id="+kodas;
+            MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+            mySqlCommand.Parameters.Add("?kliento_id", MySqlDbType.Int32).Value = kodas;
+            mySqlConnection.Open();
+            MySqlDataAdapter mda = new MySqlDataAdapter(mySqlCommand);
+            DataTable dt = new DataTable();
+            mda.Fill(dt);
+            mySqlConnection.Close();
+
+            foreach (DataRow item in dt.Rows)
+            {
+                klientas.id = Convert.ToInt32(item["kliento_id"]);
+                klientas.asmens_kodas = Convert.ToInt64(item["asmens_kodas"]);
+                klientas.vardas = Convert.ToString(item["vardas"]);
+                klientas.pavarde = Convert.ToString(item["pavarde"]);
+                klientas.amzius = Convert.ToInt32(item["amzius"]);
+                klientas.telefono_numeris = Convert.ToString(item["telefono_numeris"]);
+                klientas.elektroninis_pastas = Convert.ToString(item["elektroninis_pastas"]);
+            }
+            return klientas;
+        }
+
+        public bool addKlientas(Klientas klientas)
+        {
+            try
+            {
+                string conn = ConfigurationManager.ConnectionStrings["MysqlConnection"].ConnectionString;
+                MySqlConnection mySqlConnection = new MySqlConnection(conn);
+                string sqlquery = @"INSERT INTO "+"klientai(kliento_id,asmens_kodas,vardas,pavarde,amzius,telefono_numeris,elektroninis_pastas)VALUES(?kliento_id,?asmens_kodas,?vardas,?pavarde,?amzius,?telefono_numeris,?elektroninis_pastas);";
+                MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+                mySqlCommand.Parameters.Add("?kliento_id", MySqlDbType.Int32).Value = klientas.id;
+                mySqlCommand.Parameters.Add("?asmens_kodas", MySqlDbType.Int64).Value = klientas.asmens_kodas;
+                mySqlCommand.Parameters.Add("?vardas", MySqlDbType.VarChar).Value = klientas.vardas;
+                mySqlCommand.Parameters.Add("?pavarde", MySqlDbType.VarChar).Value = klientas.pavarde;
+                mySqlCommand.Parameters.Add("?amzius", MySqlDbType.Int32).Value = klientas.amzius;
+                mySqlCommand.Parameters.Add("?telefono_numeris", MySqlDbType.VarChar).Value = klientas.telefono_numeris;
+                mySqlCommand.Parameters.Add("?elektroninis_pastas", MySqlDbType.VarChar).Value = klientas.elektroninis_pastas;
+                mySqlConnection.Open();
+                mySqlCommand.ExecuteNonQuery();
+                mySqlConnection.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public int getNewId()
+        {
+            try
+            {
+                string conn = ConfigurationManager.ConnectionStrings["MysqlConnection"].ConnectionString;
+                MySqlConnection mySqlConnection = new MySqlConnection(conn);
+                string sqlquery = @"SELECT MAX(kliento_id) FROM klientai";
+                MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+                mySqlConnection.Open();
+                int id = (int)mySqlCommand.ExecuteScalar();
+                mySqlConnection.Close();
+                return id+1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return 0;
+            }
+        }
+
+        public void deleteKlientas(int id)
+        {
+            string conn = ConfigurationManager.ConnectionStrings["MysqlConnection"].ConnectionString;
+            MySqlConnection mySqlConnection = new MySqlConnection(conn);
+            string sqlquery = @"DELETE FROM "+"klientai where kliento_id="+id;
+            MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+            mySqlCommand.Parameters.Add("?id", MySqlDbType.Int32).Value = id;
+            mySqlConnection.Open();
+            mySqlCommand.ExecuteNonQuery();
+            mySqlConnection.Close();
+        }
+
+        public bool updateKlientas(Klientas klientas)
+        {
+            try
+            {
+                string conn = ConfigurationManager.ConnectionStrings["MysqlConnection"].ConnectionString;
+                MySqlConnection mySqlConnection = new MySqlConnection(conn);
+                string sqlquery = @"UPDATE "+"klientai a SET a.vardas=?vardas, a.pavarde=?pavarde, a.amzius=?amzius, a.telefono_numeris=?tel, a.elektroninis_pastas=?email, a.asmens_kodas=?asmens_kodas WHERE a.kliento_id=?kliento_id";
+                MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+                mySqlCommand.Parameters.Add("?kliento_id", MySqlDbType.Int32).Value = klientas.id;
+                mySqlCommand.Parameters.Add("?asmens_kodas", MySqlDbType.Int64).Value = klientas.asmens_kodas;
+                mySqlCommand.Parameters.Add("?vardas", MySqlDbType.VarChar).Value = klientas.vardas;
+                mySqlCommand.Parameters.Add("?pavarde", MySqlDbType.VarChar).Value = klientas.pavarde;
+                mySqlCommand.Parameters.Add("?amzius", MySqlDbType.Int32).Value = klientas.amzius;
+                mySqlCommand.Parameters.Add("?tel", MySqlDbType.VarChar).Value = klientas.telefono_numeris;
+                mySqlCommand.Parameters.Add("?email", MySqlDbType.VarChar).Value = klientas.elektroninis_pastas;
+                mySqlConnection.Open();
+                mySqlCommand.ExecuteNonQuery();
+                mySqlConnection.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
